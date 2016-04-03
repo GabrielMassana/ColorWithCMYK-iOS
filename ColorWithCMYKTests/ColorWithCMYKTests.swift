f7da7b237935d6f80f468213fbde320734e8d939
GabrielMassana <gabrielmassana@gmail.com>
2016-04-03 21:44:54 +0200

    Shared scheme Test Scheme

---
+++ ColorWithCMYKTests/ColorWithCMYKTests.swift
@@ -0,0 +1,24 @@
+//
+//  ColorWithCMYKTests.swift
+//  ColorWithCMYKTests
+//
+//  Created by GabrielMassana on 03/04/2016.
+//  Copyright © 2016 GabrielMassana. All rights reserved.
+//
+
+import XCTest
+
+@testable import ColorWithCMYK
+
+class ColorWithCMYKTests: XCTestCase {
+    
+    override func setUp() {
+        
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+}
