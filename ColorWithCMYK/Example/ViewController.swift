751fd3b437a5afe2b25c7fac9c49fee43ae88a52
GabrielMassana <gabrielmassana@gmail.com>
2016-04-03 22:11:09 +0200

    added example

--- ColorWithCMYK/Example/ViewController.swift
+++ ColorWithCMYK/Example/ViewController.swift
@@ -11,15 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let redColor = UIColor.colorWithCMYK(c: 0.0, m: 1.0, y: 1.0, k: 0.0)
+        let greenColor = UIColor.colorWithCMYK(c: 1.0, m: 0.0, y: 1.0, k: 0.0)
+        let blueColor = UIColor.colorWithCMYK(c: 1.0, m: 1.0, y: 0.0, k: 0.0)
+        let blackColor = UIColor.colorWithCMYK(c: 0.0, m: 0.0, y: 0.0, k: 1.0)
+        let whiteColor = UIColor.colorWithCMYK(c: 0.0, m: 0.0, y: 0.0, k: 0.0)
+        
+        print(redColor)
+        print(greenColor)
+        print(blueColor)
+        print(blackColor)
+        print(whiteColor)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
